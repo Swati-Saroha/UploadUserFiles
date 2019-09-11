bd9b5be62ec7e19e503a7e5f1246fd5837de4e20
swati saroha <swatisaroha321@gmail.com>
2019-09-11 19:15:59 +0530

    copy and paste files using grails

---
+++ src/integration-test/resources/GebConfig.groovy
@@ -0,0 +1,35 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxDriver
+
+environments {
+
+    // run via “./gradlew -Dgeb.env=chrome iT”
+    chrome {
+        driver = { new ChromeDriver() }
+    }
+
+    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
+    chromeHeadless {
+        driver = {
+            ChromeOptions o = new ChromeOptions()
+            o.addArguments('headless')
+            new ChromeDriver(o)
+        }
+    }
+
+    // run via “./gradlew -Dgeb.env=firefox iT”
+    firefox {
+        driver = { new FirefoxDriver() }
+    }
+
+  /*  development {
+        uploadFolder = "/home/ongraph/Documnets/"
+    }
+    test {
+        uploadFolder = "/home/ongraph/Documnets/"
+    }
+    production {
+        uploadFolder = "/home/ongraph/Documnets/"
+    }*/
+}
